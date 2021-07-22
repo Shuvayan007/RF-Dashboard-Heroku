3d5ee8113af2b3766086c210853d30583702a9ea
Shuvayan <shuvayanpal2000@gmail.com>
2021-07-22 12:38:42 +0530

    From Desktop:22-07-2021

--- SVR Dashboard V-1.0.py
+++ SVR Dashboard V-1.0.py
@@ -6,36 +6,17 @@ from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
-import seaborn as sns
-import subprocess
 
-def draw_meshgrid():
-    a = np.arange(start=X[:, 0].min() - 1, stop=X[:, 0].max() + 1, step=0.01)
-    b = np.arange(start=y.min() - 1, stop=y.max() + 1, step=0.01)
-    # b = np.arange(start=X[:, 1].min() - 1, stop=X[:, 1].max() + 1, step=0.01)
-
-    XX, YY = np.meshgrid(a, b)
-
-    input_array = np.array([XX.ravel(), YY.ravel()]).T
-    # input_array = np.array([XX.ravel(), YY.ravel()]).T
-
-    return XX, YY, input_array
 
 X,y=load_diabetes(return_X_y=True)
 df=pd.DataFrame(X)
 df['target']=y
 X=df[9].values.reshape(442,1)
 y=df['target'].values
-# (n_samples=300,noise=0.2,random_state=20)
 X_train,X_test,y_train,y_test=train_test_split(X,y)
 
 
 st.sidebar.markdown("# Support Vector Regression Dashboard")
-
-# dataset=st.sidebar.selectbox(
-#     'Dataset',
-#     ('DS1','DS2')
-# )
 
 tuning=st.sidebar.radio(
     'Hyper Parameter',
@@ -48,28 +29,31 @@ if tuning=='Tuning':
         ('rbf','linear','poly','sigmoid','precomputed')
     )
 
-    # if kernel=='poly':
-    #     degree=st.sidebar.slider('Degree of the polynomial kernel function (Have to realise)',value=3)
-    # else:
-    #     degree=0
-    #
-    # gamma = st.sidebar.selectbox(
-    #     'Gamma',
-    #     ('scale', 'auto', 'float')
-    # )
-    #
-    # if gamma=='float':
-    #     gamma=st.sidebar.number_input('Gamma Value (Have to think about range)')
-    #
-    # if kernel=='poly' or kernel=='sigmoid':
-    #     coef0=st.sidebar.number_input('Coefficient 0 (Have to think about name, range)')
-    # else:
-    #     coef0=0.0
-    #
-    # tol=st.sidebar.number_input('Tolerance (Have to think about range)',value=0.001)
-    #
-    # C=st.sidebar.number_input('C (Regularization Parameter) (Have to think about range <=1)',min_value=0.01,value=1.0)
-    #
+    if kernel=='poly':
+        degree=st.sidebar.slider('Degree of the polynomial',value=3, max_value=10)
+    else:
+        degree=0
+
+    if kernel=='rbf' or kernel=='poly' or kernel=='sigmoid':
+        gamma = st.sidebar.selectbox(
+            'Gamma',
+            ('scale', 'auto', 'float')
+        )
+
+        if gamma=='float':
+            gamma=st.sidebar.number_input('Gamma Value',value=0.01)
+    else:
+        gamma='scale'
+
+    if kernel=='poly' or kernel=='sigmoid':
+        coef0=st.sidebar.number_input('Coefficient 0')
+    else:
+        coef0=0.0
+
+    tol=st.sidebar.number_input('Tolerance',value=0.001,format="%.3f",min_value=0.001,step=0.001)
+
+    C=st.sidebar.number_input('C (Regularization Parameter)',min_value=0.01,value=1.0)
+
     # epsilon=st.sidebar.number_input('Epsilon Value',min_value=0.01,value=0.1)
     #
     # shrinking=st.sidebar.selectbox(
@@ -94,17 +78,15 @@ if tuning=='Tuning':
     #
     # max_iter=st.sidebar.slider('Maximum Iteration',value=-1,max_value=500,min_value=-1)
 
-    clf=SVR(kernel)
-    #,degree,gamma,coef0,C,epsilon,shrinking,cache_size,verbose, max_iter)
+    clf=SVR(kernel,degree,gamma,coef0,tol,C)
+    #,epsilon,shrinking,cache_size,verbose, max_iter)
 else:
     clf=SVR()
 
 
 fig,ax=plt.subplots()
 
-# sns.scatterplot(X,y)
 ax.scatter(X,y)
-# ax.scatter(X.T[0],X.T[1],cmap='viridis')
 orig=st.pyplot(fig)
 
 
@@ -114,22 +96,12 @@ if st.sidebar.button('Run Algorithm'):
         clf.fit(X_train,y_train)
         y_pred=clf.predict(X_test)
 
-        XX,YY,input_array=draw_meshgrid()
-        # labels=clf.predict(input_array)
-        # labels = clf.predict(input_array)
 
-        # ax.plot(y_pred,labels.reshape(XX.shape))
         x=np.linspace(X.min(),X.max(),X.shape[0]).reshape(X.shape)
-        # print(x.shape)
-        # x.reshape(X_test.shape)
         ax.plot(x,clf.predict(x),color='red')
-        # ax.contourf(XX,YY,labels.reshape(XX.shape),alpha=0.3,cmap='rainbow')
 
         plt.xlabel('Col1')
         plt.ylabel('Col2')
         orig=st.pyplot(fig)
         st.sidebar.subheader("Mean absolute error of the model: "+str(round(mean_squared_error(y_test,y_pred),2)))
     st.success("Done!")
-# subprocess = subprocess.Popen(clf,shell=True, stdout=subprocess.PIPE)
-# subprocess_return = subprocess.stdout.read()
-# print(subprocess_return)
